74a5fa72a9b2f72c64fa3fabb5d7678aa4aa78d4
Alex Campo <abrettc@gmail.com>
2022-10-04 15:18:23 -0400

    added seeder file and approuter

---
+++ backend/seeders/20221004180009-listings.js
@@ -0,0 +1,223 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(
+      'listings',
+      [
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Belfair Golf Club',
+          imageUrl: '',
+          description: `Arnold Palmer built one of his greatest courses at Featuring two of Tom Fazio's most impressive designs, the Belfair Golf Club stands out amongst private clubs in the Hilton Head golf scene. 
+          "Belfair is the ultimate in golf. It's Shadow Creek quality," Fazio has said of the club. "I'd stake my reputation on the fact that Belfair is as good as any golf course you will ever see or play." 
+          The West course came first, and Fazio was immediately rehired for the East course. Each course offers its own challenge, but Fazio worked with the natural landscape to ensure that both courses would have the ambiance of its location. Each features grandiose pines, marshland and native grasses. 
+          "Fazio has really done some of his more impressive work at Belfair," said a TravelGolf.comSpring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.`,
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1100,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Colleton River Golf Course',
+          imageUrl: '',
+          description:
+            'The quality of the Colleton River Club courses is matched only by their beauty. World-renowned designers, Jack Nicklaus and Pete Dye bring Colleton River Members 36 masterfully planned holes amidst sweeping water views. The 6-hole executive Par 3 course from Bruce Borland adds yet another option to the Colleton River golf experience. Warm up and practice takes place on expansive driving ranges, putting greens, chipping greens and bunkers. The recently completed Learning Center, located in the Practice Park, helps Members take their game to the next level. Two Clubhouses are equally impressive and complement the courses, offering a variety of spaces to dine, relax and take in the landscaped vistas and awe-inspiring natural beauty of Colleton River Club.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 300,
+          bidIncrement: 50,
+          approximateValue: 1300,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Chechessee Creek Golf Club',
+          imageUrl: '',
+          description:
+            'At Chechessee Creek Club, golf is at the center of an unparalleled club experience. As the only Bill Coore & Ben Crenshaw-designed golf course in South Carolina, members enjoy a course that rewards players adept at strategy, precision, and short game equally with those with power and length.With seasoned caddies available to share their knowledge of the course, members enjoy a walk among the scenic Lowcountry landscape of majestic Live oaks and long-needle pines with stunning marsh views dotted along the course.',
+          itemSummary:
+            'Golf for 4 including cart. Players will be responsible for their caddie fees ($100 per bag).',
+          startingBid: 300,
+          bidIncrement: 50,
+          approximateValue: 1300,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'Old Tabby Golf Links @ Spring Island',
+          imageUrl: '',
+          description:
+            'Arnold Palmer built one of his greatest courses at Spring Island. Known as one of his top 2 designs, Old Tabby Links at Spring Island is fully immersed in the landscape. With no homes visible from the course, golfers are in their element throughout the game. Paved cart paths have been forsaken for dirt walkways, preventing runoff and accommodating walkers. With a casual atmosphere and a no rush attitude, golf here is truly a leisure pursuit. In 2019, for the 20th year running, Old Tabby has earned recertification in the Audubon Cooperative Program for its long standing commitment to environmental stewardship throughout the course design.',
+          itemSummary: 'Golf for 4 including cart',
+          startingBid: 250,
+          bidIncrement: 50,
+          approximateValue: 1000,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('users', null, {})
+  }
+}
